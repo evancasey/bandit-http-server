086e14339bdc353964d66b2e237a6173e7ac3ddb
evancasey <evan.casey92@gmail.com>
2013-07-02 19:05:46 -0700

    added update and delete calls

--- api.py
+++ api.py
@@ -3,28 +3,33 @@ from flask import request, session, url_for, jsonify, abort, make_response
 from app import app, db
 from models import armKeys, banditKeys
 import pdb
+import json
+import ast
 
 #---------------------------------------------
 # api
 # --------------------------------------------
 
-# Create a new bandit
+# create a new bandit
 @app.route("/api/v1.0/bandits", methods = ['POST'])
 def createBandit():
 
+	# if not a json request throw a 400 error
 	if not request.json:
 		abort(400)
 
-	# Initialize the arms
+	# if params not given throw a 401 error
+	if not ['name','arm_count','algo_type','horizon_type','horizon_value','epsilon'] in request.json:
+		abort(401)
+
 	arms = {}
-	arms_key_arr = []
+	arm_keys_arr = []
 	for i in range(request.json['arm_count']):
-		arms_key_arr.append(armKeys())
-		arms[arms_key_arr[i]] = {
+		arm_keys_arr.append(armKeys())
+		arms[arm_keys_arr[i]] = {
 			'value': 0
 		}
-	
-	# Initialize the bandit
+
 	bandit = {
 		'name': request.json['name'],
 		'arm_count': request.json['arm_count'],
@@ -37,29 +42,69 @@ def createBandit():
 
 	db.hset("bandits", banditKeys(), bandit)
 
-	return jsonify( { "name" : bandit['name'], "bandit_id" : db.hget("unique_ids", "bandit"), "arm_ids": arms_key_arr} ), 201
+	return jsonify( { "name" : bandit['name'], "bandit_id" : db.hget("unique_ids", "bandit"), "arm_ids" : arm_keys_arr} ), 201
 
 
-# Look up by bandit ID
+# look up by bandit ID
 @app.route("/api/v1.0/bandits/<int:bandit_id>", methods = ['GET'])
 def getBandit(bandit_id):
-	bandit = db.hget("bandits", bandit_id)
+
+	bandit = ast.literal_eval(db.hget("bandits", bandit_id))
+
+	# if bad ID, throw 404 error
 	if len(bandit) == 0:
 		abort(404)
-	return jsonify( {'bandit': bandit} )
+
+	# TODO: add some other stuff here
+	return jsonify( {'name': bandit['name'], 'arms': bandit['arms']} )
 
 
-# #TODO: add this
-# @app.route("/api/v1.0/bandits/<int:bandit_id>"), methods = ['PATCH'])
-# def updateBandit():
+# update an existing bandit
+@app.route("/api/v1.0/bandits/<int:bandit_id>", methods = ['PUT'])
+def updateBandit(bandit_id):
+
+	bandit = ast.literal_eval(db.hget("bandits", bandit_id))
+
+	# if bad ID, throw 404 error
+	if len(bandit) == 0:
+		abort(404)
+
+	# if not a json request throw a 400 error
+	if not request.json:
+		abort(404)
+
+	bandit['name'] = request.json.get('name', bandit['name'])
+	bandit['arm_count'] = request.json.get('arm_count', bandit['arm_count'])
+	bandit['algo_type'] = request.json.get('algo_type', bandit['algo_type'])
+	bandit['horizon_type'] = request.json.get('horizon_type', bandit['horizon_type'])
+	bandit['horizon_value'] = request.json.get('horizon_value', bandit['horizon_value'])
+	bandit['epsilon'] = request.json.get('epsilon', bandit['epsilon'])
+
+	# TODO: add some other stuff here
+	return jsonify( { bandit_id : bandit } )
 
 
-# #TODO: add this
-# @app.route("/api/v1.0/bandits/<int:bandit_id>"), mothads = ['DELETE'])
-# def deleteBandit():
+# delete an existing bandit
+@app.route("/api/v1.0/bandits/<int:bandit_id>", methods = ['DELETE'])
+def deleteBandit(bandit_id):
+	
+	bandit = db.hget("bandits", bandit_id)
+
+	# if bad ID, throw 404 error
+	if len(bandit) == 0:
+		abort(404)
+
+	db.hdel("bandits", bandit_id)
+
+	return jsonify( { 'result': True } )
 
 
-# Error handling
+# error handling
 @app.errorhandler(404)
 def not_found(error):
 	return make_response(jsonify( { 'Error': 'Not found'} ), 404)
+
+# error handling
+@app.errorhandler(401)
+def missing_params(error):
+	return make_response(jsonify( { 'Error': 'Missing parameters'} ), 401)
--- helper.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-def armKeys():
-	db.hincrby("unique_ids", "arm")
-	return db.hget("unique_ids", "arm")
-
-def banditKeys():
-	db.hincrby("unique_ids", "bandit")
-	return db.hget("unique_ids", "bandit")

e05db2491e08baebbfe76207c4d4b45fb24a23af
evancasey <evan.casey92@gmail.com>
2013-07-10 17:57:58 -0700

    tests

--- tests/client_tests.py
+++ tests/client_tests.py
@@ -1,22 +1,30 @@
-import os
-import imp
+import os	
 import sys
 import pdb
 import unittest
 import tempfile
-from app import db
+from launch import db
 
 sys.path.insert(0, '../client/')
 from client import BanditClient
 
+class BanditUnitTest(unittest.TestCase):
 
-class BanditClientTestCase(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		db.flushdb() #clear the db
+		self.db = db	
+		self.client = BanditClient()				
 
-	# runs before every
-	def setUp(self):		
-		self.db = db
-		self.client = BanditClient()		
-		self.db.flushdb() #clear the db
+	@classmethod
+	def tearDownClass(self):
+		pass
+
+class BanditClientTests(BanditUnitTest):
+	
+	@classmethod
+	def setUp(self):
+		rv = self.client.bandit_create(name = "my experiment", arm_count=4)	
 
 	def test_bandit_create(self):
 
@@ -33,15 +41,19 @@ class BanditClientTestCase(unittest.TestCase):
 	def test_bandit_get(self):
 
 		# test get bandit works with good params
-		# rv = self.client.bandit_get(1)
-		# assert isinstance(rv, dict)
-		# assert rv['name'] == "my experiment"
+		
+		rv = self.client.bandit_get(1)
+		print type(rv)
+		assert isinstance(rv, dict)
+		assert rv['name'] == "my experiment"
 
 		# test get band
 		pass
 
+	@classmethod
 	def tearDown(self):
 		pass
+
 
 
 if __name__ == '__main__':
